98bfb08bf8431afd4e6537640a7c1c2948882095
wang <wangmingzhang@outlook.com>
2018-01-22 16:53:40 +0800

    em

--- lib/ThreadsManager.h
+++ lib/ThreadsManager.h
@@ -54,7 +54,7 @@ public:
     }
 
     void join() {
-        MSLEEP(1);//休眠500us,等待被唤醒的线程启动
+        MSLEEP(10);//休眠500us,等待被唤醒的线程启动
         while (_running != 0){
             MSLEEP(1);//休眠500us之后再查询有无进程再运行
         }

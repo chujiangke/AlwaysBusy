00fc33be82aefc5d6e64639c2a874451d850c288
Lab Ubuntu <wangmingzhang@outlook.com>
2018-01-25 09:55:58 +0800

    2

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ typedef struct _Point{
     _Point(int xx,int yy):x(xx),y(yy){};
     int x;
     int y;
-}Postion;//要处理的数据的类型定义，我这里是处理点
+}Position;//要处理的数据的类型定义，我这里是处理点
 
 std::mutex mCout;               //打印互斥锁
-ThreadsManager<Postion> *manager; //线程管理器
+ThreadsManager<Position> *manager; //线程管理器
 
 void callback(int i){
     while(true){
@@ -19,36 +19,36 @@ void callback(int i){
             break;
         }
         //取点
-        Postion point = manager->pop();
+        Position point = manager->pop();
 
         //处理数据
         MSLEEP(i*100);//休眠一段时间,代表处理数据时间
         mCout.lock();
-        printf("Postion(%d,%d)\n",point.x,point.y);
+        printf("Position(%d,%d)\n",point.x,point.y);
         mCout.unlock();
     }
 }
 
 void demo(){
-    manager = new ThreadsManager<Postion>(4);
+    manager = new ThreadsManager<Position>(4);
     manager->create(callback);
     for(int i=0;i<5;i++){
         for(int j=0;j<5;j++){
-           manager->add(Postion(i,j));
+           manager->add(Position(i,j));
         }
     }
     manager->join();//等待处理完所有points里面的数据
     std::cout << "emmm" << std::endl;
     for(int i=5;i<10;i++){
         for(int j=5;j<10;j++){
-            manager->add(Postion(i,j));
+            manager->add(Position(i,j));
         }
     }
     manager->join();
     std::cout << "emmmmmm" << std::endl;
     for(int i=10;i<15;i++){
         for(int j=10;j<15;j++){
-            manager->add(Postion(i,j));
+            manager->add(Position(i,j));
         }
     }
     manager->kill();//退出线程

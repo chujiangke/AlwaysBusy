f8fe68b0829392a9ca8a12996152cad870302ef1
Lab Ubuntu <wangmingzhang@outlook.com>
2018-01-27 16:40:35 +0800

    2

--- main.cpp
+++ main.cpp
@@ -9,9 +9,13 @@ typedef struct _Point{
     int y;
 }Position;//要处理的数据的类型定义，我这里是处理点
 
-std::mutex mCout;               //打印互斥锁
+std::mutex mCout;                   //打印互斥锁
 ThreadsManager<Position> *manager; //线程管理器
 
+/**
+ * 在子线程执行的函数模板
+ * @param i 子线程的的id，范围是1~n
+ */
 void callback(int i){
     while(true){
         manager->wait();    //有资源的话会立即返回，没有资源会阻塞
@@ -21,7 +25,7 @@ void callback(int i){
         //取点
         Position point = manager->pop();
 
-        //处理数据
+        //处理数据，可以自定义的部分
         MSLEEP(i*100);//休眠一段时间,代表处理数据时间
         mCout.lock();
         printf("Position(%d,%d)\n",point.x,point.y);
@@ -30,23 +34,23 @@ void callback(int i){
 }
 
 void demo(){
-    manager = new ThreadsManager<Position>(4);
-    manager->create(callback);
-    for(int i=0;i<5;i++){
+    manager = new ThreadsManager<Position>(4);  //新建线程管理对象
+    manager->create(callback);                  //创建线程
+    for(int i=0;i<5;i++){                       //添加点
         for(int j=0;j<5;j++){
            manager->add(Position(i,j));
         }
     }
-    manager->join();//等待处理完所有points里面的数据
+    manager->join();                    //等待处理完所有points里面的数据
     std::cout << "emmm" << std::endl;
-    for(int i=5;i<10;i++){
+    for(int i=5;i<10;i++){              //再次添加点
         for(int j=5;j<10;j++){
             manager->add(Position(i,j));
         }
     }
-    manager->join();
+    manager->join();                    //等待处理完
     std::cout << "emmmmmm" << std::endl;
-    for(int i=10;i<15;i++){
+    for(int i=10;i<15;i++){             //再次添加数据
         for(int j=10;j<15;j++){
             manager->add(Position(i,j));
         }

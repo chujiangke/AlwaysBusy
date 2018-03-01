d05703d9d05b01a5489f0465da2908827b826d2a
Lab Ubuntu <wangmingzhang@outlook.com>
2018-03-01 11:22:12 +0800

    2

--- lib/ThreadsManager.h
+++ lib/ThreadsManager.h
@@ -3,11 +3,16 @@
 
 #include <thread>
 #include <list>
-#include <unistd.h>
 #include "Semaphore.h"
 #include "tools.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#define SLEEP(x)  (Sleep(x)) //休眠xms
+#else
+#include <unistd.h>
 #define SLEEP(x) (usleep((x*100))) //休眠x*100us
+#endif
 
 using namespace SEM;
 
@@ -23,14 +28,20 @@ private:
     bool _run;                  //是否继续运行子线程
     std::list<DataType> _dataList;      //数据链表
     std::mutex _mutexDataList;          //数据链表互斥锁
+    long _coreCount;             //CPU核心数量
 
 public:
+    /**
+     * 构造函数，获取逻辑核心数量和新建用来管理资源的信号量
+     * @param count 线程数量
+     */
     explicit ThreadsManager(int count):threads(nullptr),_run(true) {
-        ASSERT(count>0 && count<12);//
+        _coreCount = getCoreCount();
+        ASSERT(count>0 && count<_coreCount,"线程数量不正确");//
         _count = count;
         _running = count;
         semaphore = new Semaphore("semaphore",0);
-        ASSERT(semaphore != nullptr);
+        ASSERT(semaphore != nullptr,"新建信号量失败");
     }
 
     ~ThreadsManager() {
@@ -40,6 +51,11 @@ public:
             delete semaphore;
     }
 
+    /**
+     * 创建子线程
+     * @param f 子线程中执行的函数，函数的形式需要满足一定的格式
+     * @return 没啥用
+     */
     bool create(void (*f)(int)) {
         threads = new std::thread[_count];
         if(threads == nullptr){
@@ -49,13 +65,20 @@ public:
         for (int i=0;i<_count;i++){
             threads[i] = std::thread(f,i);
         }
-        return false;
+        return true;
     }
 
+    /**
+     * 返回是否继续运行的标志_run
+     * @return 还要继续运行线程_run的值为true，返回的是true，否则返回false
+     */
     bool run() {
         return _run;
     }
 
+    /**
+     * 等待子线程处理完数据
+     */
     void join() {
         SLEEP(5);//休眠500us,等待被唤醒的线程启动
         while (_running != 0){
@@ -63,6 +86,9 @@ public:
         }
     }
 
+    /**
+     * wait操作
+     */
     void wait() {
         _mutex.lock();
         _running--;
@@ -73,10 +99,16 @@ public:
         _mutex.unlock();
     }
 
+    /**
+     * signal操作
+     */
     void signal() {
         semaphore->signal();
     }
 
+    /**
+     * 通知所有线程退出
+     */
     void kill() {
         join();
         _run = false;
@@ -86,6 +118,10 @@ public:
         }
     }
 
+    /**
+     * 向队列中添加数据
+     * @param data 数据
+     */
     void add(DataType data) {
         _mutexDataList.lock();
         _dataList.emplace_back(data);
@@ -93,12 +129,34 @@ public:
         signal();
     }
 
+    /**
+     * 从队列中取出数据
+     * @return 数据
+     */
     DataType pop() {
         _mutexDataList.lock();
         DataType data = _dataList.front();   //取出第一个
         _dataList.pop_front();             //删除第一个
         _mutexDataList.unlock();
         return data;
+    }
+
+private:
+    /**
+     * 获取CPU逻辑核心数量
+     * @return 核心数量
+     */
+    long getCoreCount()
+    {
+        long count = 1; // 至少一个
+#ifdef _WIN32
+        SYSTEM_INFO si;
+        GetSystemInfo(&si);
+        count = si.dwNumberOfProcessors;
+#else
+        count = sysconf(_SC_NPROCESSORS_CONF);
+#endif
+        return count;
     }
 };
 
--- lib/tools.h
+++ lib/tools.h
@@ -5,13 +5,15 @@
 #include <iostream>
 #include <string>
 
-#define ASSERT(x) assertion(__FILE__, __func__, __LINE__, x)
+#define ASSERT(condition,error_message) ((condition)?0:assertion(__FILE__, __func__, __LINE__,error_message))
 
-inline void assertion(std::string filename, std::string function,int line, bool isAllRight){
-    if(!isAllRight){
-        std::cerr<<filename+" "+function+" "+std::to_string(line)+">>  "+"Assertion Failed"<<std::endl;
-        exit(-1);
-    }
+inline int assertion(std::string filePath, std::string function,int line, std::string info){
+    //获取文件名
+    unsigned long pos=filePath.find_last_of('/');
+    std::string filename(filePath.substr(pos+1));
+    std::string err = filename+" "+function+" "+std::to_string(line)+">>  "+info;
+    //抛出错误
+    throw std::runtime_error(err);
 }
 
 #endif //DETECTFINAL_TOOLS_H

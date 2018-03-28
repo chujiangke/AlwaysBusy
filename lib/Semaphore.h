44f1f7f0f94ccb99fccbeae5ac2d1faafbc4492e
Lab Ubuntu <wangmingzhang@outlook.com>
2018-03-28 20:40:52 +0800

    改用vector作为线程的容器

--- lib/Semaphore.h
+++ lib/Semaphore.h
@@ -17,57 +17,61 @@ namespace SEM {
         std::string _name;                      //信号量名字
 
     public:
-        explicit Semaphore(std::string name, int count = 0) :
-                _name(std::move(name)), _count(count) {}
+        explicit Semaphore(std::string name, int value = 0) :
+                _name(std::move(name)), _count(value) {}
 
         /**
-         * 阻塞一个线程，相当于信号量机制里面的P操作
+         * 相当于信号量机制里面的P操作.
+         * _count大于0(有资源)时,函数会立即返回,否则会阻塞调用此函数的线程.
          */
         void wait() {
             std::unique_lock<std::mutex> lock(_mutex);
             //std::cout<<_name+":"<<_count<<std::endl;
-            if (--_count < 0) { // _count is not enough ?
-                _condition.wait(lock); // suspend and wait ...
+            if (--_count < 0) { // 资源不够
+                _condition.wait(lock); // 阻塞
             }
         }
 
         /**
-         * 阻塞一个线程，但如果等待时间超过seconds指定的值，会唤醒所有阻塞线程
+         * 相当于信号量机制里面的P操作.
+         * _count大于0(有资源)时,函数会立即返回,否则会阻塞调用此函数的线程.
+         * 但如果等待时间超过seconds指定的值，会唤醒所有阻塞线程.
          * @param ms 等待时间(ms)
          */
         void wait(int ms) {
             assert(ms > 0);
             std::unique_lock<std::mutex> lock(_mutex);
             //std::cout<<_name+":"<<_count<<std::endl;
-            if (--_count < 0) {                            // _count is not enough ?
-                std::cv_status status = _condition.wait_for(lock,
-                                                            std::chrono::milliseconds(ms));
-                if (status == std::cv_status::timeout)   //timeout
+            if (--_count < 0) {                            // 资源不够
+                std::cv_status status
+                        = _condition.wait_for(lock, std::chrono::milliseconds(ms));
+                if (status == std::cv_status::timeout)   // 超时
                 {
                     std::cout << _name + ":timeout" << std::endl;
                     _count = 0;
-                    _condition.notify_all();
+                    _condition.notify_all();//唤醒所有阻塞线程
                 }
             }
         }
 
         /**
-         * 随机唤醒一个线程，相当于信号量机制里面的V操作
+         * 如果有阻塞的线程,则随机唤醒一个线程，相当于信号量机制里面的V操作.否则
+         * 立即返回.
          */
         void signal() {
             std::lock_guard<std::mutex> lock(_mutex);
-            if (++_count <= 0) {         // have some thread suspended ?
-                _condition.notify_one(); // notify one !
+            if (++_count <= 0) {
+                _condition.notify_one();
             }
         }
 
         /**
-         * 唤醒阻塞的所有线程
+         * 如果有线程阻塞,唤醒阻塞的所有线程;否则立即返回.
          */
         void signalAll() {
             std::lock_guard<std::mutex> lock(_mutex);
-            while (++_count <= 0) {      // have some thread suspended ?
-                _condition.notify_one(); // notify one !
+            while (++_count <= 0) {
+                _condition.notify_one();
             }
             _count = 0;
         }
